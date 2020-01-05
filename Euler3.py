52cdfe106eff093c889d8785853f567dd49b4941
Dan <danielbentley@sky.com>
2020-01-05 20:07:10 +0000

    First attempt finished

---
+++ Euler3.py
@@ -0,0 +1,17 @@
+def largest_prime_factor(number):
+
+    factor = number
+
+    counter = 2
+
+    while counter <= factor ** 0.5:
+        if factor % counter == 0:
+            factor = factor / counter
+            counter -= 1
+        counter += 1
+
+    if factor >= 2:
+        return factor
+
+
+print(largest_prime_factor(600851475143))
